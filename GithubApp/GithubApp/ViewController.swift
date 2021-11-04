51b63688ff9da3e31b0b03693ec5730987809631
Sett Park <park9368@naver.com>
2021-11-04 14:02:55 +0900

    init: 프로젝트 초기화

---
+++ GithubApp/GithubApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GithubApp
+//
+//  Created by 박정하 on 2021/11/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
